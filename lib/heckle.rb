5e67bbc479aeb7df2820302128872e9d330a3b52
Pete Higgins <pete@peterhiggins.org>
2012-07-08 14:27:54 -0700

    Don't need this anymore.

--- lib/heckle.rb
+++ lib/heckle.rb
@@ -13,14 +13,6 @@ end
 
 class Sexp
   # REFACTOR: move to sexp.rb
-  def deep_each(&block)
-    self.each_sexp do |sexp|
-      block[sexp]
-      sexp.deep_each(&block)
-    end
-  end
-
-  # REFACTOR: move to sexp.rb
   def each_sexp
     self.each do |sexp|
       next unless Sexp === sexp
